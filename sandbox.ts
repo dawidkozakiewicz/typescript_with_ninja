01bd7d4da38ff76922414ed6a8be23b440c77eff
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-06-04 19:54:24 +0200

    basics

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,14 @@
-const character = 'mario';
-
-console.log(character);
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-  console.log(input);
-});
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var area = function (diameter) {
+  return diameter * Math.PI;
+};
+// console.log(area('hello'));
+console.log(area(7.5));
