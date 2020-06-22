3c9cd84e4f32c2c84d1b18ab99eadd73bd651286
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-06-22 22:47:25 +0200

    plaing with JS

--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -2,10 +2,10 @@ import { HasFormatter } from '../interfaces/HasFormatter.js';
 
 export class Invoice implements HasFormatter {
   constructor(
-    readonly client: string, 
-    private details: string, 
+    readonly client: string,
+    private details: string,
     public amount: number,
-  ){}
+  ) { }
 
   format() {
     return `${this.client} owes £${this.amount} for ${this.details}`;
