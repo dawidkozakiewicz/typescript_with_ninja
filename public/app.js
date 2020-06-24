bc8ea599ac2004af79ab826e8302f7e5fcbb6a48
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-06-24 12:38:39 +0200

    generics

--- public/app.js
+++ public/app.js
@@ -11,7 +11,6 @@ const amount = document.querySelector('#amount');
 // list template instance
 const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
-const test = document.querySelector('.test');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -21,6 +20,32 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
     list.render(doc, type.value, 'end');
 });
+// GENERICS
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+//let docTwo = addUID('shaun');
+console.log(docOne.name);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 1,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
